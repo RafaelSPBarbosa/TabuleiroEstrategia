1376c9a138b3a60aae59c9034e59ae72f98e9395
RafaelSPBarbosa <rafaspb11@gmail.com>
2016-04-23 20:39:55 -0300

    Iniciando a implementação de interpolação
    Não funcionou, portanto eu aumentei o valor do SendRate para 29, teremos
    que criar um verdadeiro sistema de interpolação no futuro.

---
+++ EstrategiaTabuleiro/Assets/NetworkInterpolation.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetworkInterpolation : NetworkBehaviour {
+
+    [SyncVar]
+    private Vector3 syncPos;
+
+    [SerializeField]
+    Transform myTransform;
+    [SerializeField]
+    float lerpRate = 15;
+
+    void Start()
+    {
+        myTransform = this.transform;
+    }
+
+    void FixedUpdate()
+    {
+        TransmitPosition();
+        LerpPosition();
+    }
+
+    void LerpPosition()
+    {
+        if (!isLocalPlayer)
+        {
+            myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * lerpRate);
+        }
+    }
+
+    [Command]
+    void CmdProvidePositionToServer(Vector3 pos)
+    {
+        syncPos = pos;
+    }
+
+
+    [ClientCallback]
+    void TransmitPosition()
+    {
+
+        CmdProvidePositionToServer(myTransform.position);
+    }
+}
