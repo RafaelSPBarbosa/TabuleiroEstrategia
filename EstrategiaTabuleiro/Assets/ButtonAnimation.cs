5da23fb744b7b4001c3aee6d55c9b73074c91e83
Pokegabi <gaio.gabi@gmail.com>
2016-04-24 13:27:18 -0300

    Implementação Inicial de menu
    Falta implementação de interface visual para o server e opções.

---
+++ EstrategiaTabuleiro/Assets/ButtonAnimation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonAnimation : MonoBehaviour {
+
+    public void AbrirBotao()
+    {
+        GetComponent<Animator>().SetBool("MouseSobre", true);
+    }
+
+    public void FecharBotao()
+    {
+        GetComponent<Animator>().SetBool("MouseSobre", false);
+
+
+    }
+}
