680df7512b8d6f6eb894368ab3e359b73ed29840
RafaelSPBarbosa <rafaspb11@gmail.com>
2016-06-06 21:52:53 -0300

    Implementação das musicas de fundo
    - Adicionado Musica de Menu
    - Adicionado Musica de Jogo

---
+++ EstrategiaTabuleiro/Assets/Initialization.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Initialization : MonoBehaviour {
+
+    public GameObject MusicManager;
+
+	// Use this for initialization
+	void Start () {
+
+        DontDestroyOnLoad(MusicManager);
+        StartCoroutine("LoadMenu");
+	}
+
+    IEnumerator LoadMenu()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ EstrategiaTabuleiro/Assets/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip MenuMusic , GameMusic;
+
+    public AudioSource AS;
+
+	// Update is called once per frame
+	void Update () {
+
+        if (SceneManager.GetActiveScene().name == "Menu")
+        {
+            if (AS.clip != MenuMusic)
+            {
+                AS.clip = MenuMusic;
+                AS.Play();
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "Game")
+        {
+            if (AS.clip != GameMusic)
+            {
+                AS.clip = GameMusic;
+                AS.Play();
+            }
+        }
+	}
+}
