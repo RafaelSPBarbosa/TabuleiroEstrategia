d61bc866bb38cf2d23f1b705f119d8e9112c9710
Jay Peet <jaytechmedia@googlemail.com>
2016-07-07 23:03:42 +0100

    Quick Update

--- EstrategiaTabuleiro/Assets/Scripts/MatchMaking.cs
+++ EstrategiaTabuleiro/Assets/Scripts/MatchMaking.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class MatchMaking : MonoBehaviour {
 
+    public string www_data = "";
+
 	// Use this for initialization
 	void Start () {
-	
+        Debug.Log("Searching for Matches");
+        StartCoroutine("GetServers");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    IEnumerator GetServers()
+    {
+        string url = "http://www.jaytechmedia.com/autem/list.php";
+        WWW www = new WWW(url);
+        yield return www;
+        www_data = www.text;
+        Debug.Log(www.text);
+        //Convert JSON into unity useable array
+       // JsonUtility.FromJson(www.text, MatchMakingData);
+        yield return null;
+    }
 }
